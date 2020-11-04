c07ed7780d7b4e6ef4008b76bf6b590b28bd402f
Ankit Kr Shukla <ankit.shukla@sunarctechnologies.com>
2020-11-04 13:19:58 +0530

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule ,PreloadAllModules} from '@angular/router';
 import { LoginComponent } from './account/login/login.component';
 import { SignupComponent } from './account/signup/signup.component';
-import { customerComponent } from './customer/customer.component';
+import { UserinterfaceComponent } from './customer/userinterface/userinterface.component';
+
 
 const routes: Routes = [
-  { path: '', component: customerComponent,loadChildren: () => import('./customer/customer.module').then(m => m.customerModule) },
+ 
+  { path: '',pathMatch:'full',component: UserinterfaceComponent,loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: '**', redirectTo: '/' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes,
+      {
+       preloadingStrategy: PreloadAllModules
+      }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
