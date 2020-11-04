8c6913938f963718430e23c1055f23ad767bb496
Ankit Kr Shukla <ankit.shukla@sunarctechnologies.com>
2020-11-04 13:17:02 +0530

    route configured for customer panel

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SaleComponent } from './sale/sale.component';
 import { MensproductComponent } from './mensproduct/mensproduct.component';
@@ -7,18 +7,35 @@ import { DistributerComponent } from './distributer/distributer.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { DashboardComponent } from '../admin/dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HeadertabComponent } from './headertab/headertab.component';
 import { FootertabComponent } from './footertab/footertab.component';
+import { PrivacypoliceComponent } from './privacypolice/privacypolice.component';
+import { SingleproductComponent } from './singleproduct/singleproduct.component';
+import { UserinterfaceComponent } from './userinterface/userinterface.component';
+import { BrowserModule } from '@angular/platform-browser';
 
-const   routeCoustomer:any=[ { path: '', component: HomeComponent}]
+
+const   routeCoustomer:Routes=[ 
+  { path: '', component: HomeComponent,pathMatch:"full",outlet:'customer'},
+{ path: 'policy', component: PrivacypoliceComponent,outlet:'customer'},
+{ path: 'contact', component: ContactusComponent},
+{ path: 'distributor', component: DistributerComponent},
+{ path: 'mens', component: MensproductComponent},
+{ path: 'womens', component: WomensproductComponent},
+{ path: 'sale', component: SaleComponent},
+{ path: 'productview', component: SingleproductComponent}
+]
 
 @NgModule({
-  declarations: [FootertabComponent,HeadertabComponent, HomeComponent,SaleComponent,MensproductComponent,WomensproductComponent,
-    DistributerComponent,ContactusComponent,DashboardComponent],
+  declarations: [ UserinterfaceComponent,
+   HomeComponent,SaleComponent,MensproductComponent,WomensproductComponent,
+   DistributerComponent,ContactusComponent,DashboardComponent, PrivacypoliceComponent,HeadertabComponent,FootertabComponent
+  ],
   imports: [
-    CommonModule,
-    RouterModule.forRoot(routeCoustomer),
-  ]
+    CommonModule,        
+    RouterModule.forChild(routeCoustomer)
+  ],exports:[RouterModule],
+    schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
-export class customerModule { }
+export class CustomerModule { }
---
+++ src/app/customer/privacypolice/privacypolice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-privacypolice',
+  templateUrl: './privacypolice.component.html',
+  styleUrls: ['./privacypolice.component.css']
+})
+export class PrivacypoliceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
