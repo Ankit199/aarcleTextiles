cfc855461b14883055a7e34933ed4b0f0b0f024f
Ankit Kr Shukla <ankit.shukla@sunarctechnologies.com>
2020-11-05 17:47:22 +0530

    custom.js issue resolved for home page

--- src/app/customer/home/home.component.ts
+++ src/app/customer/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { SharedStorage } from 'ngx-store';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,10 +9,17 @@ import { SharedStorage } from 'ngx-store';
 export class HomeComponent implements OnInit {
  
   @SharedStorage('cart') _cartItem: Array<any> = [];
-
-  constructor() { }
+scriptUrl:string='../../../assets/js/custom.js';
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+   this.addJsToElement(this.scriptUrl)
   }
-
+  addJsToElement(src: string): HTMLScriptElement {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    this.renderer.appendChild(document.body, script);
+    return script;
+  }
 }
