3a06b69bbe97dfe045f76e6bbab542b7910bea66
Ankit Kr Shukla <ankit.shukla@sunarctechnologies.com>
2020-11-16 17:54:02 +0530

    auth service path change

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/shared/auth.service';
+import { AuthService } from 'src/app/Backend/Service/auth.service';
+
 
 @Component({
   selector: 'app-login',
