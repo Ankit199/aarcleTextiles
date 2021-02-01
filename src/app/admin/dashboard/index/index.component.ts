c730cb3cd9d35967ea680284b7009f1317670027
Ankit Kr Shukla <ankit.shukla@sunarctechnologies.com>
2021-02-02 02:00:59 +0530

    admin paneel addded

--- src/app/admin/dashboard/index/index.component.ts
+++ src/app/admin/dashboard/index/index.component.ts
@@ -11,7 +11,10 @@ import { SpinnerService } from 'src/app/shared/@spinner/spinner.service';
   styleUrls: ['./index.component.css'],
 })
 export class IndexComponent implements OnInit {
-  EcommerceForm: any;
+  EcommerceForm: any={
+    Sku:""
+  };
+  isValidFormSubmitted:boolean=false;
   colorModel: string = '';
   EcommerceID: string | null;
   list: any = [];
@@ -31,6 +34,8 @@ export class IndexComponent implements OnInit {
       { name: '32', checked: false },
       { name: '34', checked: false },
     ];
+    this.EcommerceForm.Sku=this.getUniqueId(3);
+
     // this.ref.detectChanges();
   }
 
@@ -58,17 +63,26 @@ export class IndexComponent implements OnInit {
     }
   };
   addEcommerce(EcommerceForm: NgForm) {
-    console.table(EcommerceForm.value);
+   // console.table(EcommerceForm.value);
     this.firestoreService.add(AddnewProductFirePath, EcommerceForm.form.value);
-    alert('Data Added');
+    this.toast.success('Data Saved Successfully.', 'Success!');
+  }
+  getUniqueId(parts: number): string {
+    const stringArr = [];
+    for(let i = 0; i< parts; i++){
+      // tslint:disable-next-line:no-bitwise
+      const S4 = (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+      stringArr.push(S4);
+    }
+    return stringArr.join('-');
   }
   uploadFile(event: any, type: string) {
     this.spinner.showLoader();
     const id = Math.random().toString(36).substring(2);
     if (event && type) {
       if (type === 'image') {
-        alert('Image uploading..');
-        // this.toastr.info("Image uploading..", "Wait!");
+       // alert('Image uploading..');
+         this.toast.info("Image uploading..", "Wait!");
         if (event.target.files && event.target.files[0]) {
           var filesAmount = event.target.files.length;
           for (let i = 0; i < filesAmount; i++) {
@@ -100,19 +114,17 @@ export class IndexComponent implements OnInit {
     }
   }
 
-  spanColorClick = () => {
-    (document.getElementById('colour') as HTMLInputElement).click();
-  };
+  
   colorchange = () => {
-    this.colorModel = (document.getElementById(
-      'colour'
-    ) as HTMLInputElement).value;
-    console.log(this.colorModel);
-    (document.getElementById(
-      'color_front'
-    ) as HTMLInputElement).style.backgroundColor = (document.getElementById(
-      'colour'
-    ) as HTMLInputElement).value;
+    // this.colorModel = (document.getElementById(
+    //   'colour'
+    // ) as HTMLInputElement).value;
+    // console.log(this.colorModel);
+    // (document.getElementById(
+    //   'color_front'
+    // ) as HTMLInputElement).style.backgroundColor = (document.getElementById(
+    //   'colour'
+    // ) as HTMLInputElement).value;
   };
   addColor = (): void => {
     if (this.colorModel !== '') {
